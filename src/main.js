db49a54f2f5144e3ff48e7d38026ce3bf4f06fb8
mattcheng <mattcheng20080205@gmail.com>
2024-12-09 17:30:09 +0800

    testing

--- src/main.js
+++ src/main.js
@@ -1,18 +1,34 @@
 import plugin from '../plugin.json';
 
-class AcodePlugin {
+const loader = acode.require("loader");
+const toast = acode.require('toast');
+
+class AcodeWakatime {
 
   async init() {
-
-  }
-
+  	this.showWelcomeMessage();
+  	}
+  	
+  showWelcomeMessage() {
+        // 例如：顯示一個歡迎提示
+        toast("", 5000);
+        }
+        
+  downloadLatestWakatime() {
+  	try {
+          const response = await fetch("https://api.github.com/repos/wakatime/wakatime-cli/releases/latest");
+          
+          if(!response.ok){
+          	toast("error download wakatime", 3000)
+          }
+          
   async destroy() {
-
+      
   }
 }
 
 if (window.acode) {
-  const acodePlugin = new AcodePlugin();
+  const acodePlugin = new AcodeWakatime();
   acode.setPluginInit(plugin.id, async (baseUrl, $page, { cacheFileUrl, cacheFile }) => {
     if (!baseUrl.endsWith('/')) {
       baseUrl += '/';
